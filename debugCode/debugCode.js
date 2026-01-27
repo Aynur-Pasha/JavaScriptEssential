2bdf1358355499ca887b113f5ac5df2a0d6f97e4
Aynur-Pasha <aynur.pascha@gmail.com>
2026-01-27 02:56:57 -0500

    add debugging example

--- debugCode/debugCode.js
+++ debugCode/debugCode.js
@@ -2,10 +2,23 @@ function performOperation() {
     // Get user input from input fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
+    let operation = document.getElementById("operation").value;
 
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
-        let result = multiply(num1, num2);
+        let result;
+        if (operation === "mult") {
+            result = multiply(num1, num2);
+        } else if (operation === "add") {
+            result = num1 + num2;
+        } else if (operation === "div") {
+            if (num2==0) {
+                displayResult('Cannot divide by 0')
+            } else {
+                result = num1 / num2;
+            }
+        }
+        
         // Display the result
         displayResult(result);
     } else {
